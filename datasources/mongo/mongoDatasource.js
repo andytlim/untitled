e89c59f9d43baebaf24740925c7dc00da2d7794c
Andy Lim <andytlim@gmail.com>
2016-08-16 00:52:57 -0400

    Updated to new folder structure. Added react sample

--- datasources/mongo/mongoDatasource.js
+++
@@ -1,3 +0,0 @@
-var mongodb = require('mongodb');
-
-module.exports = mongodb.MongoClient.connect('mongodb://knockknock:gitgud2016@ds023902.mlab.com:23902/untitled');
--- datasources/youtube/youtubeDatasource.js
+++
@@ -1,6 +0,0 @@
-var Youtube = require('youtube-node');
-
-var youtube = new Youtube();
-youtube.setKey('AIzaSyCLpDjTdey4mrFeg2GGgSlN6YR0Mzf2h0w');
-
-module.exports = youtube;
