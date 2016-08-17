97e27cdded0849a74d411cb085683e605b6a02f0
Kirny <kirnyx3@gmail.com>
2016-08-17 19:45:03 -0400

    added mongodb driver and added inserting

--- routes/youtube.js
+++ routes/youtube.js
@@ -19,10 +19,40 @@ router.post('/:Id', function(req, res, next) {
             if(err) {
                 res.send("Sorry that isn't a valid youtube ID :(");
             } else {   
+			
+				var mongodb = require('mongodb');
+				var MongoClient = mongodb.MongoClient;
+				var url = 'mongodb://knockknock:gitgud2016@ds023902.mlab.com:23902/untitled'
+				
+				MongoClient.connect(url, function (err, db) {
+					  if (err) {
+						console.log('Unable to connect to the mongoDB server. Error:', err);
+					  } else {
+						//HURRAY!! We are connected. :)
+						console.log('Connection established to', url);
+
+						// do some work here with the database.
+                        db.collection('media').insertOne( {
+                            "mid":(Math.round(Math.random() * (9999999-1000000) + 1000000)),
+                            "mediaTypeId":"1",
+                            "metadata":{"title":snippetinfo.title,
+                                        "description":snippetinfo.description}
+                        }, function(err, result) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log("Send to MongoDB!");
+                            }      
+                        });
+
+						//Close connection
+						db.close();
+					  }
+					});
                 
                 var snippetinfo = data.items[0].snippet;   
                 res.send("Title is : " + snippetinfo.title + "<br />" +
-                        "Description is :" + snippetinfo.description); 
+                        "Description is : " + snippetinfo.description); 
                 
             }  
      });
