57e0c585dbfa40edb3ebe60a65cbcf23833fab12
Kirny <kirnyx3@gmail.com>
2016-08-29 21:15:01 -0400

    Added more components

--- public/react/jsx/sample-react-youtube.jsx
+++ public/react/jsx/sample-react-youtube.jsx
@@ -1,14 +1,33 @@
+var videolist = React.createClass({
+    render:function() {
+        return (
+            <div>
+                <h1>Your video list!</h1>
+            </div>
+        )
+    }
+});
+
 
 var YoutubeSection = React.createClass({
+  getInitialState: function() {
+    return {
+      videos: ["hi"]
+    };
+  },
+
   render: function() {
     return (
         <div>
             <h1> This is the youtube test page. </h1>  
             <p>Please enter a youtube ID: </p>
-            <form>
-                <input type="text"></input>
-                <input type="submit"></input>
-            </form>
+            <input type="text"></input>
+            <button>Click me!</button>
+            
+            <videolist />
+            
+            <p>yo</p>
+            
         </div>
     )
   }
